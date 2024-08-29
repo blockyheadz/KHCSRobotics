d0562eabc15e949f4cbd0273e2a93054d29f2133
blockyheadz <gaveragehumanlivingonearth@gmail.com>
2024-08-29 14:43:45 -0400

    Using both joysticks to control robot

--- testing/src/main.cpp
+++ testing/src/main.cpp
@@ -84,7 +84,7 @@ void opcontrol() {
 
 		// Arcade control scheme
 		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_Left_X);  // Gets the turn left/right from right joystick
+		int turn = master.get_analog(ANALOG_LEFT_X);  // Gets the turn left/right from right joystick
 		left_wheel.move(turn);                      // Sets left motor voltage
 		right_wheel.move(dir);                     // Sets right motor voltage
 		pros::delay(20);                               // Run for 20 ms then update
