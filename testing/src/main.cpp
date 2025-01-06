5a0ec49c6b94c45f73c102d3c5f41a0fc868fad1
Batman <omarbrothers170@gmail.com>
2025-01-06 14:32:55 -0500

    anything

--- testing/src/main.cpp
+++ testing/src/main.cpp
@@ -1,169 +1,105 @@
 #include "main.h"
 
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
+// Global objects for controllers and motors
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+pros::Motor frontLeft(20); 
+pros::Motor frontRight(11);  
+pros::Motor backLeft(18);
+pros::Motor backRight(13);
+pros::Motor midLeft(19);
+pros::Motor midRight(12);
+pros::Motor intake(7); // Assuming intake is motor 7
+
+// Arrays for drivetrain motors (left and right)
+pros::Motor leftDrive[] = {frontLeft, midLeft, backLeft};
+pros::Motor rightDrive[] = {frontRight, midRight, backRight};
+
+// Center button callback for LCD display
 void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
+    static bool pressed = false;
+    pressed = !pressed;
+    if (pressed) {
+        pros::lcd::set_text(2, "I was pressed!");
+    } else {
+        pros::lcd::clear_line(2);
+    }
 }
 
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
+// Initialization
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor frontLeft(20); 
-	pros::Motor frontRight(11);  
-	pros::Motor backLeft(18);
-	pros::Motor backRight(13);
-	pros::Motor midLeft(19);
-	pros::Motor midRight(12);
-
-	pros::lcd::initialize();
-	pros::Motor leftDrive[] = {frontLeft, midLeft, backLeft};
-	pros::Motor rightDrive[] = {frontRight, midRight, backRight};
-
-	pros::lcd::register_btn1_cb(on_center_button);
+    pros::lcd::initialize();
+    pros::lcd::set_text(1, "Hello PROS User!");
+    pros::lcd::register_btn1_cb(on_center_button);
 }
 
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
+// Disabled mode function
 void disabled() {}
 
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
+// Competition initialization function
 void competition_initialize() {}
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
+// Autonomous Drivetrain Movement (Simple Example)
 void autonomous() {
+    int speed = 100; // Speed for autonomous movement
+    int distance = 1000; // Arbitrary tick value for distance
 
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor frontLeft(20); 
-	pros::Motor frontRight(11);  
-	pros::Motor backLeft(18);
-	pros::Motor backRight(13);
-	pros::Motor midLeft(19);
-	pros::Motor midRight(12);
+    // Move forward for 'distance' ticks
+    for (pros::Motor left : leftDrive) {
+        left.move_relative(distance, speed);
+    }
+    for (pros::Motor right : rightDrive) {
+        right.move_relative(distance, speed);
+    }
 
-	pros::lcd::initialize();
-	pros::Motor leftDrive[] = {frontLeft, midLeft, backLeft};
-	pros::Motor rightDrive[] = {frontRight, midRight, backRight};
-	
-	int speed = 50;
-	int tickToTurn = 100000;
-	while(leftDrive[0].get_position() < tickToTurn) {
-		for (pros::Motor left : leftDrive) {
-			left.move_absolute(-1 * tickToTurn, speed);
-		}
-		
-		for (pros::Motor right : rightDrive) {
-			right.move_absolute(tickToTurn, speed);
-		}
-		pros::delay(20);
-	}
+    // Wait until motors are done moving
+    while (fabs(leftDrive[0].get_position()) < distance) {
+        pros::delay(20);
+    }
+
+    // Turn by moving only the right motors
+    int turnTicks = 500;  // Arbitrary turn value
+    for (pros::Motor left : leftDrive) {
+        left.move_relative(-turnTicks, speed);
+    }
+    for (pros::Motor right : rightDrive) {
+        right.move_relative(turnTicks, speed);
+    }
+
+    // Wait until the turn is complete
+    while (fabs(leftDrive[0].get_position()) < turnTicks) {
+        pros::delay(20);
+    }
 }
 
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
+// Teleoperated (Manual) Control
+void opcontrol() {
+    while (true) {
+        // Getting joystick input from the controller
+        int forward = master.get_analog(ANALOG_LEFT_Y);  // Forward/Backward
+        int turn = master.get_analog(ANALOG_RIGHT_X);    // Turning
 
+        // Display values on LCD (optional)
+        char buffer[100];
+        sprintf(buffer, "Forward: %d | Turn: %d", forward, turn);
+        pros::lcd::set_text(1, buffer);
 
-void opcontrol()
- {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor frontLeft(20); 
-	pros::Motor frontRight(11);  
-	pros::Motor backLeft(18);
-	pros::Motor backRight(13);
-	pros::Motor midLeft(19);
-	pros::Motor midRight(12);
+        // Set left and right motor power for drivetrain
+        for (pros::Motor left : leftDrive) {
+            left.move(forward + turn);
+        }
+        for (pros::Motor right : rightDrive) {
+            right.move(forward - turn);
+        }
 
-	pros::lcd::initialize();
-	pros::Motor leftDrive[] = {frontLeft, midLeft, backLeft};
-	pros::Motor rightDrive[] = {frontRight, midRight, backRight};
+        // Intake control (example: controlled by controller buttons)
+        if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
+            intake.move(100);  // Intake in
+        } else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+            intake.move(-100);  // Intake out
+        } else {
+            intake.move(0);  // Stop intake
+        }
 
-
-	int speed = 200;
-	int tickToTurn = 100000/16;
-	while(leftDrive[0].get_position() < tickToTurn) {
-		for (pros::Motor left : leftDrive) {
-			left.move_absolute(tickToTurn, speed);
-		}
-		
-		for (pros::Motor right : rightDrive) {
-			right.move_absolute(tickToTurn, speed);
-		}
-		pros::delay(20);
-	}
-
-	while (true) 
-	{
-		int turn = master.get_analog(ANALOG_RIGHT_X);
-		int power = master.get_analog(ANALOG_LEFT_Y);
-		pros::lcd::initialize();
-		char pos[] = "";
-		sprintf(pos,"This is power %i \n This is turn %i", power, turn);
-		pros::lcd::set_text(1,pos);
-		
-		//Moving forward turn right
-		
-		for (pros::Motor left : leftDrive) {
-			left.move(-1 * (power + turn));
-		}
-		for (pros::Motor right :rightDrive) {
-			right.move( (power - turn));
-		}
-		
-
-		//Moving forward and turning left
-		
-
-		
-		pros::delay(20);                               // Run for 20 ms then update
-	}
+        pros::delay(20);  // Run this loop every 20 milliseconds
+    }
 }
